eb3029edb121307d8696a4c329f8bea2e6e267c9
Shafana parveen <shafanaparveen29@gmail.com>
2026-05-04 14:15:15 +0530

    Create Steps_in_file_handling.py

---
+++ Steps_in_file_handling.py
@@ -0,0 +1,10 @@
+#Step 1st: Create and open file
+#Creating a file and writing initial student data
+
+file=open("students_details.txt","w")
+
+file.write("101,Aman,98\n")
+file.write("102,Shafana,89\n")
+file.write("103,Alia,67\n")
+
+file.close()
