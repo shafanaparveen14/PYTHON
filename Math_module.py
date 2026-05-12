9b4621a1a62dc9e3405d57bc97c4295a65ebdee6
Shafana parveen <shafanaparveen29@gmail.com>
2026-05-12 10:12:24 +0530

    Create Math_module.py

---
+++ Math_module.py
@@ -0,0 +1,39 @@
+# Math module
+#It is a built m python library for mathematical operation.
+#Important functions include Trigonometry , logarithm, constant , power operations.
+#Importants constant .
+
+#Math pi‎ = 3.142
+#Math.e Euler Number(2.71..)
+#Trigonometric Functions( All Functions are in radians)
+#Math.sin(x)——> sine
+#Math.cos(x)———> cosine
+#Math.tan(x)———>tangent
+# Degree to radian conversion 
+# Radian conversion to degree
+#      math.radians(angle)
+# Degree conversion of radians 
+    # math.degrees(value)
+
+# Power and roots——>
+#Math.pow(x,y)—>x^y    , Example Area of circle=(Pi r^2)
+#                       =(math.pi)(math.pow(r,2))
+
+# math.sqrt(x)—>square root
+# [ ] Logarithm Functions
+# Math.log(x)——> natural log—>lnx——>logex
+#Math.log 10(x)—>log10(x)
+
+
+# Rounding Functions
+# Math.ceil(x)——> round up——>example—>math.ceil(5.45)——> 6
+# Math.floor(x)——>round down —>example—>math.floor(5.45)——>5
+# Factorial ——> math.factorial (n)—>n!
+
+#Round ()-->It is used to round numbers
+# syntax round (numbers, digits)
+#Numbers->value to round
+# digits->numbers to decimal palces
+# x= round (4.6),print(x) output->5
+#x=round(3.14159)->3.14
+# rounding writing-->4.1->4
